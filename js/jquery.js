f5b2f3ecaa19450c077e14a83449b3a3e33470a4
董聿凌 <gb2013429@gmail.com>
2022-06-06 00:18:01 +0800

    Jquery

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,12 @@
-$(document).ready(() => {
-        $('.qa-xy').click(function (e) {
-                $('.ga-text').hide();
-            });
-
-    });
+$(function() {
+        $('.qa-text').hide();
+        // $(".qa-xy").on("click", function (e) {
+        //         $('.qa-text').slideToggle(1000);
+        // })
+    })
+    $('.qa-title').on("click", function (e) {
+        e.preventDefault();
+                    //點選item-title後第一個會變色
+        $('.qa-title').first().toggleClass('active'); // 注意：active 不用加上 
+        $('.qa-text').first().slideToggle();
+    })
