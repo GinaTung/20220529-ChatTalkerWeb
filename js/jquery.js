1866a90c36dd09a8d015edbf20c1d985b00342b8
董聿凌 <gb2013429@gmail.com>
2022-06-07 00:34:43 +0800

    jquery20220606

--- js/jquery.js
+++ js/jquery.js
@@ -1,12 +1,24 @@
 $(function() {
         $('.qa-text').hide();
-        // $(".qa-xy").on("click", function (e) {
-        //         $('.qa-text').slideToggle(1000);
-        // })
+        $('#basic1').toggleClass('btn-bg')
     })
     $('.qa-title').on("click", function (e) {
-        e.preventDefault();
-                    //點選item-title後第一個會變色
-        $('.qa-title').first().toggleClass('active'); // 注意：active 不用加上 
-        $('.qa-text').first().slideToggle();
+        $(this).siblings('.qa-text').slideToggle(1000);
+        $(this).find('h4').toggleClass('active');
+        $(this).find('.y').toggle();
     })
+
+    $('.b2').on("click", function (e) {
+        // $(this).find('a').toggle();
+        $('#basic2').toggleClass('btn-bg')
+        $('#basic1').toggleClass('btn-bg')
+        $('.co-pb').text('15000');
+        $('.co-h2').text('9000');
+
+    })
+    $('.b3').on("click", function (e) {
+        $('#basic3').toggleClass('btn-bg')
+        $('#basic2').toggleClass('btn-bg')
+        $('.co-pb').text('20000')
+    })
+
